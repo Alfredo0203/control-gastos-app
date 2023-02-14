267abaff964f5a64e536ae2cb38d9572d7b05c51
Alfredo Torres <alfredotorres.niu@gmail.com>
2023-02-14 08:35:32 -0600

    Formulario para gastos agregado

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,6 +16,32 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
         </div>
         <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
             <legend>Gasto</legend>
+            <div className="campo">
+              <label htmlFor="nombre">Nombre gasto</label>
+              <input type="text" id='nombre' placeholder='Ingrese nombrer del gasto'/>
+            </div>
+
+            <div className="campo">
+              <label htmlFor="cantidad">Cantidad</label>
+              <input type="text" id='cantidad' placeholder='Ingrese cantidad: Ej. 300'/>
+            </div>
+
+            <div className="campo">
+              <label htmlFor="categoria">Categoria</label>
+              <select name="" id="categoria" >
+                <option value="">-- Seleccione categoria</option>
+                <option value="ahorro">Ahorro</option>
+                <option value="comida">Comida</option>
+                <option value="casa">Casa</option>
+                <option value="gastos">Gastos varios</option>
+                <option value="ocio">Ocio</option>
+                <option value="salud">Salud</option>
+                <option value="Subscripciones">Subscripciones</option>
+
+              </select>
+            </div>
+
+            <input type="submit" value="Agregar gasto" />
         </form>
      </div>
   )
