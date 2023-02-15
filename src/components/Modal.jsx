bdb00a849c4d44d56d25834e25c6fb6dc86976ec
Alfredo Torres <alfredotorres.niu@gmail.com>
2023-02-14 23:05:32 -0600

    Creando el state para los gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,18 @@
 import CerrarModalBtn from '../img/cerrar.svg'
+import { useState } from 'react'
 const Modal = ({setModal, animarModal, setAnimarModal}) => {
 
+  const [nombre, setNombre] = useState('');
+  const [cantidad, setCantidad] = useState(0);
+  const [categoria, setCategoria] = useState('');
     const handleCerrarModal = () => {
         setAnimarModal(false)
         setTimeout(() => {
             setModal(false)
           }, 500);
-        
-
     }
+
+
   return (
     <div className="modal">
         <div className='cerrar-modal'>
@@ -18,17 +22,20 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
             <legend>Gasto</legend>
             <div className="campo">
               <label htmlFor="nombre">Nombre gasto</label>
-              <input type="text" id='nombre' placeholder='Ingrese nombrer del gasto'/>
+              <input type="text" id='nombre' placeholder='Ingrese nombrer del gasto' 
+              value={nombre} onChange={e => setNombre(e.target.value)}/>
             </div>
 
             <div className="campo">
               <label htmlFor="cantidad">Cantidad</label>
-              <input type="text" id='cantidad' placeholder='Ingrese cantidad: Ej. 300'/>
+              <input type="text" id='cantidad' placeholder='Ingrese cantidad: Ej. 300'
+              value={cantidad} onChange={e => setCantidad(e.target.value)}/>
             </div>
 
             <div className="campo">
               <label htmlFor="categoria">Categoria</label>
-              <select name="" id="categoria" >
+              <select name="" id="categoria" 
+              value={categoria} onChange={e => setCategoria(e.target.value)}>
                 <option value="">-- Seleccione categoria</option>
                 <option value="ahorro">Ahorro</option>
                 <option value="comida">Comida</option>
